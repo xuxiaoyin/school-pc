73f161bece6d4c77a7677aea2dbf93ab7508688a
xuxiaoyin <279238850@qq.com>
2019-03-26 09:09:46 +0800

    user center

--- js/main.js
+++ js/main.js
@@ -32,6 +32,17 @@ $(function(){
    })
    
    
+   var iNow1=0;
+   function init(iNow1){
+   	$('.tab1 a').removeClass('active').eq(iNow1).addClass('active');
+   	$('.tab-list1').css('display','none').eq(iNow1).css('display','block');
+   }
+   init(iNow1);
+   $('.tab1 a').click(function(){
+   	init($(this).index())
+   })
+   
+   
    var size=14;
 	$('.tobig').click(function(){
 		if(size<20){
@@ -43,4 +54,11 @@ $(function(){
 		size--;
 		$('.article-content').children().css('fontSize',size+'px')
 	})
+	
+	$("input[name=firendType]").click(function(){
+	    var firendType = $(this).val();
+	    alert(firendType);
+	});
+	
+	
 })
